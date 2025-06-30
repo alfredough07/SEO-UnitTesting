ef78ee24d0223df1cc57d874e0fda32d3a28830f
Alfredo Zuniga <alfredo.zunigadiaz@outlook.com>
2025-06-30 17:22:39 +0000

    Fix final style issues in test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -2,6 +2,7 @@ import unittest
 
 from calc import Calc
 
+
 class TestCalc(unittest.TestCase):
 
     def setUp(self):
@@ -17,10 +18,10 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(self.calc.mul(2, 3), 6)
 
     def test_div(self):
-        self.assertEqual(self.calc.div(10,5), 2)
+        self.assertEqual(self.calc.div(10, 5), 2)
 
     def test_pow(self):
-        self.assertEqual(self.calc.pow(2,2), 4)
+        self.assertEqual(self.calc.pow(2, 2), 4)
 
     def test_sqrt(self):
         self.assertEqual(self.calc.sqrt(36), 6)
